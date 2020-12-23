df9ceb4c4cda90cb5c279e4db1ee9aefc7ea981f
lelo451 <ra98995@uem.br>
2020-12-23 13:36:58 -0300

    Stage 2/6: Almost an octopus

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -1,20 +1,12 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(10 + " is equal to 0b" + convertToBinary(10));
-    }
-
-    public static String convertToBinary(int number) {
-        String str = "";
-        if (number == 0) {
-            str = "0";
-        } else {
-            while (number > 0) {
-                str = number % 2 + str;
-                number /= 2;
-            }
-        }
-        return str;
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String radix = Integer.toString(num, 8);
+        System.out.println(radix.charAt(radix.length() - 1));
     }
 }
