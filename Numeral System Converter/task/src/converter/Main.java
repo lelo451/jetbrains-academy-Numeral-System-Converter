efedc3d50ba67cb4f62a97daf612e60a36127670
lelo451 <ra98995@uem.br>
2020-12-23 13:42:29 -0300

    Stage 3/6: Convert decimals

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        String radix = Integer.toString(num, 8);
-        System.out.println(radix.charAt(radix.length() - 1));
+        long num = sc.nextLong();
+        int radix = sc.nextInt();
+        switch (radix) {
+            case 2:
+                System.out.println("0b" + Long.toString(num, radix));
+                break;
+            case 8:
+                System.out.println("0" + Long.toString(num, radix));
+                break;
+            case 16:
+                System.out.println("0x" + Long.toString(num, radix));
+                break;
+        }
     }
 }
