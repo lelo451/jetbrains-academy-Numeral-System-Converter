807ff8801ca024b8896fc82c94b433c02dedd165
lelo451 <ra98995@uem.br>
2020-12-23 13:52:42 -0300

    Stage 4/6: Any which radix

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -5,18 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long num = sc.nextLong();
-        int radix = sc.nextInt();
-        switch (radix) {
-            case 2:
-                System.out.println("0b" + Long.toString(num, radix));
-                break;
-            case 8:
-                System.out.println("0" + Long.toString(num, radix));
-                break;
-            case 16:
-                System.out.println("0x" + Long.toString(num, radix));
-                break;
+        int radixOfNum = sc.nextInt();
+        String numInString = sc.next();
+        int radixTarget = sc.nextInt();
+
+        long num;
+        if (radixOfNum == 1) {
+            num = numInString.length();
+        } else {
+            num = Long.parseLong(numInString, radixOfNum);
+        }
+
+        if (radixTarget == 1) {
+            for (int i = 0; i < num; i++) {
+                System.out.print(1);
+            }
+        } else {
+            String numTarget = Long.toString(num, radixTarget);
+            System.out.print(numTarget);
         }
     }
 }
